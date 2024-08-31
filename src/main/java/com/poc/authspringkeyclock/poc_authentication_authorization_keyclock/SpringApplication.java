d25e7b5b116629224e1817de2dbe482c16f452fe
Vinicius Pacheco <viniciuspacheco.devjava@gmail.com>
2024-08-31 15:44:10 -0300

    Iniciando projeto spring de autenticacao e autorizacao com keyclock

---
+++ src/main/java/com/poc/authspringkeyclock/poc_authentication_authorization_keyclock/SpringApplication.java
@@ -0,0 +1,12 @@
+package com.poc.authspringkeyclock.poc_authentication_authorization_keyclock;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/authspringkeyclock/poc_authentication_authorization_keyclock/SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.authspringkeyclock.poc_authentication_authorization_keyclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
